b8733fb548d81b43cfe92adabde00663e533c80c
ramalakshmi2020 <ramalakshmi.n@gmail.com>
2021-06-07 21:42:26 -0700

    Initial Commit

---
+++ UWPSoundBar/Model/Sound.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundBar.Model
+{
+    public enum SoundCategory
+    {
+        Animals,
+        Cartoons,
+        Taunts,
+        Warnings
+    }
+    class Sound
+    {
+        public string Name { get; set; }
+        public SoundCategory category { get; set; }
+        public string AudioFile { get; set; }
+        public string ImageFile { get; set; }
+
+    }
+}
